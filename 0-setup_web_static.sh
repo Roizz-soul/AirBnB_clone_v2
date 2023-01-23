da2abe98043f978ee6f557b7b18ccbd3fa707179
Oluremi Abraham <olurmeiabraham9@gmail.com>
2023-01-23 16:47:03 +0000

    task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# A script to set up my web servers for the deployment of web_static
+
+apt-get update
+apt-get -y install nginx
+mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+echo 'Hello World!' > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+echo '# server config
+server {
+    listen 80;
+
+    location /hbnb_static {
+        root /data/web_static/current/;
+        alias /data/web_static/current/hbnb_static;
+    }
+}' | sudo tee -a /etc/nginx/sites-available/default > /dev/null
+nginx -s reload
