db781f8e7797c2397c239cbb503b478aceac979c
Oluremi Abraham <olurmeiabraham9@gmail.com>
2023-01-25 16:45:38 +0000

    try 5 ii

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -11,39 +11,39 @@ $word = '<html>
 $update = "\\\n\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}"
 
 package { 'nginx':
-	ensure   => 'present',
-	provider => 'apt'
+  ensure   => 'present',
+  provider => 'apt'
 }
 
 file { '/data/web_static/releases/test':
-    ensure  => 'directory',
-    recurse => true,
+  ensure  => 'directory',
+  recurse => true,
 }
 
 file { '/data/web_static/shared':
-    ensure  => 'directory',
-    recurse => true,
+  ensure  => 'directory',
+  recurse => true,
 }
 
 file { '/data/web_static/releases/test/index.html':
-	ensure  => 'present',
-	content => ${word},
+  ensure  => 'present',
+  content => $word,
 }
 
 file { '/data/web_static/current':
-    ensure => 'link',
-    target => '/data/web_static/releases/test'
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
 }
 
 file { '/data':
-    ensure  => 'directory',
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-    recurse => true,
+  ensure  => 'directory',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  recurse => true,
 }
 
 exec { 'configure and restart nginx':
-    command => 'sudo sed -i "55i $update" /etc/nginx/sites-available/default && 
-                sudo service nginx restart',
-    provider => shell,
+  command  => 'sudo sed -i "55i $update" /etc/nginx/sites-available/default && 
+               sudo service nginx restart',
+  provider => shell,
 }
