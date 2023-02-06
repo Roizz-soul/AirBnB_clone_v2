f2acfac36aeda8cb4b002710b4eb9bb4a9c5a30a
Oluremi Abraham <olurmeiabraham9@gmail.com>
2023-02-06 19:00:08 +0000

    Task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Starting a flask web app. that listens on a host and port
+   displays contents"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """display content on net"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
