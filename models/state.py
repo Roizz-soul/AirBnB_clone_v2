eb966184e28e4ddaa81c67b267a89ad5c20f8d4e
Oluremi Abraham <olurmeiabraham9@gmail.com>
2023-02-08 13:08:02 +0000

    Corr. task 7

--- models/state.py
+++ models/state.py
@@ -2,6 +2,7 @@
 """ State Module for HBNB project """
 import models
 from models.base_model import BaseModel, Base
+from models.city import City
 from os import getenv
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
