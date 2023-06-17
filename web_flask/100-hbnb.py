8ed22ce172d1d8be75a6ab25834919be602281ba
Oluremi Abraham <olurmeiabraham9@gmail.com>
2023-06-17 13:24:05 +0000

    Done with Web framework

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -5,6 +5,7 @@
 from flask import Flask, render_template
 from models import storage
 from models.amenity import Amenity
+from models.place import Place
 from models.state import State
 app = Flask(__name__)
 
@@ -15,14 +16,17 @@ def close_storage(exc):
     storage.close()
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hello_hbnb():
     """display content on net"""
     states = storage.all(State).values()
     amenities = storage.all(Amenity).values()
-    return render_template("10-hbnb_filters.html",
+    places = storage.all(Place).values()
+
+    return render_template("100-hbnb.html",
                            states=states,
-                           amenities=amenities)
+                           amenities=amenities,
+                           places=places)
 
 
 if __name__ == "__main__":
